3f5dba26bb946d4c227ef39901ff061574ea86cd
ilvas <il.vsl0110@gmail.com>
2020-11-20 14:10:40 +0300

    remove bugs

--- lib/DataWorker.dart
+++ lib/DataWorker.dart
@@ -11,22 +11,28 @@ class DataWorker {
   static void writeJSON(String name, String date, String descr) {
     getApplicationDocumentsDirectory().then(
       (e) {
+        //File("${e.path}/data.json").writeAsStringSync("[]", mode:FileMode.write);
         File file = File("${e.path}/data.json");
-        if (!file.existsSync()) file.writeAsStringSync("");
+        if (!file.existsSync()) file.writeAsStringSync("{}");
         Map<String, dynamic> data = new Map<String, dynamic>();
         if (file.readAsStringSync().length != 0) data = jsonDecode(file.readAsStringSync());
-        data[name] = {"date": date, "isChecked": "false", "description": ""};
+        data[name] = {"date": date, "isChecked": "false", "description": descr};
         file.writeAsStringSync(jsonEncode(data));
       });
   }
   static Future<List<Widget>> getTasks() async {
+    
     List<Widget> Tasks = new List<Widget>();
     Directory p = await getApplicationDocumentsDirectory();
+    //File("${p.path}/data.json").writeAsStringSync("{}", mode:FileMode.write);
+    print(File("${p.path}/data.json").readAsStringSync());
+    
     bool f = await File(p.path+"/data.json").exists();
-    if (!f) File(p.path+"/data.json").writeAsStringSync("");
+    if (!f) File(p.path+"/data.json").writeAsStringSync("{}");
     Map<String, dynamic> data = jsonDecode(File("${p.path}/data.json").readAsStringSync());
     data.keys.forEach((e) {
-      Tasks.add(WidgetOfTask(data[e]["isChecked"], e, data[e]["date"], data[e]["descr"]));
+      print(data[e]);
+      Tasks.add(WidgetOfTask(name:e, time:data[e]["date"], discription:data[e]["description"]));
     });
     return Tasks;
   }
--- lib/WidgetOfTask.dart
+++ lib/WidgetOfTask.dart
@@ -1,15 +1,107 @@
 import 'package:flutter/material.dart';
 
-class WidgetOfTask extends StatelessWidget {
-  String name, date, isChecked, descr;
-  WidgetOfTask(String isChecked, String name, String date, String descr) {
-    this.name = name;
-    this.isChecked = isChecked;
-    this.date = date;
-    this.descr = descr;
-  }
+class WidgetOfTask extends StatefulWidget{
+
+    
+  String name;
+  String time;
+  String discription;
+
+  WidgetOfTask({this.name, this.time, this.discription});
+
   @override
-  Widget build(BuildContext ctx) {
-    return Container(child: Text(this.name));
+  _Card createState() => _Card(name:name, time:time, descr:discription); 
+}
+
+class _Card extends State<WidgetOfTask>{
+  
+  bool comleted = false;
+  Image image = Image.asset("assets/rect.png");
+  Icon eleps = Icon(Icons.circle, color: Colors.blue[300],);
+
+  String name, time, descr;
+
+  _Card({this.name, this.time, this.descr});
+
+  void elepse(){
+    setState(() {
+      if(this.comleted == false){
+        this.eleps = Icon(Icons.circle, color: Colors.blue[300],);
+      }
+      else{
+        this.eleps = Icon(Icons.circle, color: Colors.green[300],);
+      }
+    
+    });
+  }
+
+  Image imageSelector(){
+    setState(() {
+      if(this.comleted == false){
+        this.image = Image.asset("assets/rect.png");
+      }
+      else{
+        this.image  = Image.asset("assets/check-mark.png");
+      }
+    });
+  }
+
+  Widget build(BuildContext context){
+
+    ListTile myList = ListTile(
+        leading: Container(child: this.image),
+        title: Text(this.name),
+        subtitle: Row(
+          children: [
+            eleps,
+            Text(this.time),
+          ],
+        ),
+        onLongPress: (){
+          setState(() {
+            this.comleted = !this.comleted;
+            elepse();
+            imageSelector();
+          });
+        },
+        onTap: (){
+          setState(() {
+            showDialog<void>(
+              context: context,
+              barrierDismissible: false,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text(this.name),
+                  content: SingleChildScrollView(
+                    child: ListBody(
+                      children: <Widget>[
+                        Text(this.descr),
+                      ],
+                    ),
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                      child: Text('ОК'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+          });
+        },
+      );
+    return Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4), 
+          border: Border.all(
+            color: Color(0xffe3e3e3), 
+            width: 1,
+            ),
+          ),
+        child: myList
+    );
   }
 }
